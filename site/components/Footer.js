027a0d6eb61d788b541408149ee49ebb2e99c543
JakieBoi77 <jakef7@icloud.com>
2024-01-21 16:35:07 -0500

    Refactored footer

--- site/components/Footer.js
+++ site/components/Footer.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer aria-label="Site Footer" className='fixed bottom-5 left-1/2 -translate-x-1/2'>
+    <footer aria-label="Site Footer" className='absolute bottom-5 left-1/2 -translate-x-1/2'>
       <Link className="flex flex-row items-center " target="_blank" href="/">
         <img className='hover:-rotate-45 transition-all duration-400' src="/images/favicon.ico"/>
         <h5 className='text-indigo-400 pl-3 font-bold hover:text-indigo-300 transition-all duration-400 '>Try It </h5>
