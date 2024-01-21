a92a4e43fabce1e2faba48b4155f6d8d6e69ede8
JakieBoi77 <jakef7@icloud.com>
2024-01-21 18:11:41 -0500

    Added logo

--- site/components/Footer.js
+++ site/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer aria-label="Site Footer" className='absolute bottom-5 left-1/2 -translate-x-1/2'>
       <Link className="flex flex-row items-center " target="_blank" href="/">
-        <img className='hover:-rotate-45 transition-all duration-400' src="/images/favicon.ico"/>
+        <img className='hover:scale-105 transition-all duration-400' src="/images/logo.png"/>
         <h5 className='text-indigo-400 pl-3 font-bold hover:text-indigo-300 transition-all duration-400 '>Try It </h5>
       </Link>
       
