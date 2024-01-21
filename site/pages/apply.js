a9558798390245d07cc404a0602acaecb7781295
Sankalpa <90166191+xSankalpaD@users.noreply.github.com>
2024-01-21 16:25:30 -0500

    Created Register Page
    front end work
    
    Contributions:
    Jake and San

--- site/pages/apply.js
+++ site/pages/apply.js
@@ -1,23 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/apply.module.css'
+import {toast} from 'react-toastify'
 
 const Apply = () => {
+  
+  const [category, setCategory] = useState('')
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+  }
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    if(!category) return toast.error("Add a category.");
+    //backend call
+    toast('You are a bitch!')
+  }
+
   return (
     <>
-      <section className=''>
-        <div  className={styles.background + " main min-h-screen flex justify-center items-center"}>
-          <div className="content form">
-              <h1 className="text-2xl font-bold text-white text-center pb-5">Apply for NewsLetter</h1>
-              <div className="">
-                  <form className="">
-                      <input className="border-1 border-gray-100 bg-white text-black shadow-lg p-2 rounded-l-lg focus:outline-none autofocus" type="email" placeholder='Enter your email' required/>
-                      <input className="bg-indigo-600 text-white p-2 rounded-r-lg" type="submit" value="Subscribe" />
-                  </form>
-              </div>
+      {/*we need the white space so that the styles are seperated by space*/}
+      <section className={styles.background + " min-h-screen flex justify-center items-center"}>{/*min h cover whole screen and center it*/}
+        <div className="main">
+          <div className="content border px-4 py-8 rounded-2xl shadow-md bg-white">
+            <h1 className="text-2xl font-bold text-center">Join the top 1% creators</h1>
+            <p className="text-center">Create Linktree for your brand</p>
+            <p className='text-center py-5 font-bold text-gray-500'>Start Building here</p>
+            <form onSubmit={handleRegister} className="flex flex-col gap-4 text-lg mt-5">
+              <input className="shadow-md border-2 px-3 py-2 rounded-md " type="text" placeholder="Social Handle" required/>
+              <input className="shadow-md border-2 px-3 py-2 rounded-md " type="email" placeholder="Enter your email" required/>
+              <input className="shadow-md border-2 px-3 py-2 rounded-md " type="password" placeholder= 'Set a password' required/>
+              
+              <h5 className='text-sm text-center'>Account Type:</h5>
+              <span className="flex">
+                <label className="flex flex-row mr-3">
+                  <input type="checkbox" className="px-2" value="Creator" checked={category==='Creator'} onChange={handleCategoryChange}/>
+                  <p className="pl-2">Creator</p>
+                </label>
+                <label className="flex flex-row mr-3">
+                  <input type="checkbox" className="px-2" value="Agency" checked={category==='Agency'} onChange={handleCategoryChange}/>
+                  <p className="pl-2">Agency</p>
+                </label>
+                <label className="flex flex-row mr-3">
+                  <input type="checkbox" className="px-2" value="Brand" checked={category==='Brand'} onChange={handleCategoryChange}/>
+                  <p className="pl-2">Brand</p>
+                </label>
+              </span>
+              <input className="bg-indigo-600 text-white py-3 rounded-lg cursor-pointer" type="submit" value="Ni Hao" />
+            </form>
           </div>
         </div>
       </section>
-    </>
+    </>    
+  
   )
 }
 
