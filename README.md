44895334920de9e29336fc00a3d868d61d95ed31
Sankalpa <chhets1@mcmaster.ca>
2024-05-06 15:04:18 -0400

    Enable adding user info to MonoDB and tweaked user interface

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Link-Tree-Clone
+
+npm install imports used:
+npm install express
+npm install mongoos
+npm install -g nodemon
+npm install jsonwebtoken
--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
 const {registerUser, loginUser} = require('./controllers/auth');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/linkTree')
     .then(() => {
         console.log(`mongodb Connected`)
     })
     .catch(err => {
-        console.log(err.message)
+        toast.error('Username or Email already in use!')
     });
 
     
