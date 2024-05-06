e5d68adcdb9a68ee4489633ba36798ae7a881e64
Sankalpa <chhets1@mcmaster.ca>
2024-05-06 00:17:49 -0400

    Final Updates

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,6 @@
+const User= require('../models/user');
+
+
 const registerUser = (req, res) => {
     res.send("Register");
 }
---
+++ server/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const {model, Schema} = mongoose;
+
+const User = new Schema({
+    name: {type: String},
+    bio: {type: String},
+    email: {type: String, required: true, unique: true},
+    avatar: {type: String},
+    password: {type: String, required: true},
+    role: {type: String, enum: ['Creator', 'Brand', 'Agency', 'admin'], default: 'Creator'},
+    handle: {type: String, required: true, unique: true},
+    links: [{
+        url: {type: String},
+        title: {type: String},
+        icon: {type: String},
+    }],
+    socialMedia:{
+        facebook: {type: String},
+        instagram: {type: String},
+        twitter: {type: String},
+        youtube: {type: String},
+        linkedin: {type: String},
+        github: {type: String},
+        pinterest: {type: String},
+    }
+}, {collection: 'user-data-linktree'});
+
+const userModel = model('userData', User);
+
+module.exports = userModel;
