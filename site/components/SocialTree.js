32328167e46d542c1fe403a88d8da29c7a0f37fd
Sankalpa <chhets1@mcmaster.ca>
2024-09-06 01:46:35 -0400

    feat: personal user links

---
+++ site/components/SocialTree.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SocialTree = () => {
+  return (
+    <div>SocialTree</div>
+  )
+}
+
+export default SocialTree
--- site/pages/[handle].js
+++ site/pages/[handle].js
@@ -1,11 +1,72 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useState, useEffect } from 'react'
+import LinkTree from '../components/LinkTree'
+import SocialTree from '../components/SocialTree'
 
 const Handle = () => {
+
+  const router = useRouter();
+  const [data, setData] = useState({});
+  const [userFound, setUserFound] = useState(false);
+
+  const [social, setSocial] = useState({
+    facebook: "",
+    twitter: "",
+    instagram: "",
+    youtube: "",
+    linkedin: "",
+    github: ""
+  })
+
+  useEffect(() => {
+    if (router.query?.handle) {
+      fetch(`http://localhost:8080/get/${router.query.handle}`)
+        .then(res => res.json())
+        .then(data => {
+          if (data.status === 'error') return toast.error(data.error);
+          if (data.status === 'success') {
+            setData(data.userData);
+            setSocial(data.socials)
+            setUserFound(true);
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    }
+  }, [router.query])
+
+  if (!userFound) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="not-found px-3 ">
+          <h1 className="font-bold text-lg">User Not found 🙁</h1>
+          <p>If you're looking for a page, double check the spelling.</p>
+          Create your own
+          <Link
+            className="bg-indigo-600 px-2 ml-2 text-white hover:bg-indigo-400 transition-all duration-500"
+            href="/apply"
+          >
+            {" "}
+            LinkTree
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <LinkTree />
+      <ShareButton />
+      <LinkTree data={data} />
+      <SocialTree social={social} />
     </div>
   )
 }
 
 export default Handle
+
+// export async function getStaticProps({params}) {
+//   const {slug} = params;
+//   const res = await fetch()
+// }
