f2de355ec876e90e07f1cc87bbdcc7a1589e0f50
Sankalpa <chhets1@mcmaster.ca>
2024-09-07 15:17:11 -0400

    Feat: Finished edit links

--- server/controllers/saveItems.js
+++ server/controllers/saveItems.js
@@ -2,6 +2,8 @@ const User = require("../models/user");
 const jwt_decode = require("jwt-decode");
 const jwt = require("jsonwebtoken");
 
+//jwt-decode does not work anymore usign jsonwebtoken
+
 const saveSocials = async (req, res) => {
   const { tokenMail, socials } = req.body;
   console.log(req.body);
@@ -33,32 +35,33 @@ const saveProfile = async (req, res) => {
     user.name = name;
     user.bio = bio;
     user.avatar = avatar;
-    user.save();
+    await user.save();
     return res.json({ message: "saved", status: "success" });
   } catch (err) {
     return res.json({ status: "error", error: err.message });
   }
 };
  
-// const saveLinks = async (req, res) => {
-//   const { tokenMail, links } = req.body;
-//   try {
-//     const decodedTokenMail = jwt_decode(tokenMail, process.env.SECRET_JWT);
-//     const email = decodedTokenMail.email;
-//     console.log(email);
-//     const user = await User.findOne({ email: email });
-//     console.log(user);
-//     const newLinks = links.map((link) => ({
-//       url: link.link.url,
-//       title: link.link.title,
-//       icon: ""
-//     }));
-//     user.links = newLinks;
-//     await user.save();
-//     return res.json({ message: "saved", status: "success" });
-//   } catch (err) {
-//     return res.json({ status: "error", error: err.message });
-//   }
-// };
+const saveLinks = async (req, res) => {
+  const { tokenMail, links } = req.body;
+  try {
+    //const decodedTokenMail = jwt_decode(tokenMail, process.env.SECRET_JWT);
+    const decodedTokenMail = jwt.verify(tokenMail, process.env.SECRET_JWT);
+    const email = decodedTokenMail.email;
+    console.log(email);
+    const user = await User.findOne({ email: email });
+    console.log(user);
+    const newLinks = links.map((link) => ({
+      url: link.link.url,
+      title: link.link.title,
+      icon: ""
+    }));
+    user.links = newLinks;
+    await user.save();
+    return res.json({ message: "saved", status: "success" });
+  } catch (err) {
+    return res.json({ status: "error", error: err.message });
+  }
+};
  
-module.exports = { saveSocials, saveProfile };
+module.exports = { saveSocials, saveProfile, saveLinks };
